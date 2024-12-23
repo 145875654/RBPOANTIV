1ae5d2f42d7720d5c0c5f0d94b7de885231dfc54
steam <steamct@yandex.ru>
2024-12-23 18:05:49 +0300

    new files

---
+++ src/main/java/com/example/rbpoantiv/RbpoAntivApplication.java
@@ -0,0 +1,13 @@
+package com.example.rbpoantiv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbpoAntivApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RbpoAntivApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rbpoantiv/RbpoAntivApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rbpoantiv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbpoAntivApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
